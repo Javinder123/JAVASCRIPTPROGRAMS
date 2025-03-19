e1f752ab094e76c58b867708debd9f10acfc63ce
Javinder123 <pangothjavinder@gmail.com>
2025-03-19 22:51:21 +0530

    day4variables

---
+++ day4.js
@@ -0,0 +1,35 @@
+// // // primitiv data type
+
+// // const seller = "BEST SELLER";
+// // const expShipping = "EXPRESS SHIPPING"
+// // const productDescription = "BLCOK PURE COTTON PRINTED ROUND"
+// // const productPrice = 3000;
+// // console.log(seller);
+// // console.log(expShipping);
+// // console.log(productDescription);
+// // console.log(productPrice);
+// const productdetailsobj = {
+//     seller : "Best Seller",
+//     exshipping : "Express Shipping",
+//     productname:"Guess",
+//     productDescription:"Black Pure Cotton Printend round.."
+
+
+// }
+// const  a = 134;
+// console.log(productdetailsobj,a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
